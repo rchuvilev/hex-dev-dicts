4daac1f5e1dc1f5e947a0ed0c050dd215b3d941b
Roman Chuvilev <rchuvilev@gmail.com>
2025-06-06 11:33:01 +0400

    Add files via upload

---
+++ dictMimetypesDevSafe.ts
@@ -0,0 +1,22 @@
+export const dictMimetypesDevSafe = {
+  '.ts': 'application/typescript',
+  '.tsx': 'application/typescript',
+  '.js': 'application/javascript',
+  '.jsx': 'application/javascript',
+  '.mjs': 'application/javascript',
+  '.cjs': 'application/javascript',
+  '.json': 'application/json',
+  '.map': 'application/json',
+  '.md': 'text/markdown',
+  '.yml': 'application/x-yaml',
+  '.yaml': 'application/x-yaml',
+  '.env': 'text/plain',
+  '.log': 'text/plain',
+  '.gitignore': 'text/plain',
+  '.dockerfile': 'text/plain',
+  '.lock': 'text/plain', // package-lock, yarn.lock, etc
+  '.config': 'text/plain',
+  '.pem': 'application/x-pem-file',
+  '.crt': 'application/x-x509-ca-cert',
+  '.key': 'application/x-pem-file',
+};
